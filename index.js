cf30a58e3dcb9925f985bb15587f2860ae2501d8
xolile2000 <xolilentanzi43@gmail.com>
2021-08-31 14:45:10 +0200

    errors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser");
+const flash = require('express-flash');
+  const session = require('express-session');
+
 const greeting = require("./greetings");
 
 const app = express();
@@ -8,34 +11,62 @@ const greetings = greeting()
 app.engine('handlebars', exphbs({ layoutsDir: "views/layouts/" }));
 app.set('view engine', 'handlebars');
 
+app.use(session({
+    secret : "<add a secret string here>",
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  // initialise the flash middleware
+  app.use(flash());
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.get("/", function (req, res) {
+    
     res.render("index",{
         greetMe: greetings.getMassage(),
         counter: greetings.greetingcounter()
     });
+
+  
+ 
+   
+
 });
+    
+   
+
 
 app.post("/greetings", function (req, res) {
+    
+   
+    var name1 = req.body.enterName;
 
-    greetings.greetName(
+    
 
-        req.body.language,
-        req.body.enterName
-    );
+    if(!name1 || name1 === undefined ){
+        req.flash('error1',"please enter name")
+    }else{
+        greetings.greetName(req.body.language,req.body.enterName)
+
+    }
+   
+   
 
 
         res.redirect("/")
 });
 
+
 app.get("/greeted",function(req,res){
-    // greetings.getNames()
-// console.log()
-res.render('greeted',{namelist : Object.keys(greetings.getNames())})
+
+res.render('greeted',{namelist : Object.keys(greetings.getNames())
+
+});
 
 });
 
